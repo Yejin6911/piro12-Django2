67b3a8d4304d4e7366de1ffb96985b887b872ad7
yejin <cdjin6911@gmail.com>
2020-01-22 18:20:41 +0900

    03 프로필 페이지 구현하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def profile(request): #setting.LOGIN_URL로 로그아웃일때는 이동
+    #request.user #django.contrib.auth.models.User(로그인되어있을 때) 또는 django.contrib.auth.models.AnonymousUser(로그인 X일 때)
+    return render(request, 'accounts/profile.html')
--- askdjango/urls.py
+++ askdjango/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     #path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
-    path('',lambda req: redirect('blog:post_list')), #URL reverse
+    path('',lambda req: redirect('blog:post_list'), name='root'), #URL reverse
 ]
