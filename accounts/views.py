6bdbecdad88394838cf640bc682975fda42a198a
yejin <cdjin6911@gmail.com>
2020-01-27 14:49:47 +0900

    13 암호 재설정 구현하기

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.views import PasswordChangeView
-from django.core.checks import messages
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from django.contrib import messages
 from django.shortcuts import render, redirect, resolve_url
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -60,3 +60,22 @@ class MyPasswordChangeView(PasswordChangeView):
 
     def form_valid(self, form):
         messages.info(self.request, '암호 변경을 완료했습니다.')
+        return super().form_valid(form)
+
+class MyPasswordResetView(PasswordResetView):
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/password_reset_form.html'
+    # email_template_name = ...
+    # html_email_template_name = ...
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 변경 메일을 발송했습니다.')
+        return super().form_valid(form)
+
+class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/password_reset_confirm.html'
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 리셋을 완료했습니다.')
+        return super().form_valid(form)
